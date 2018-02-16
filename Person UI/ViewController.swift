389004006ba1664133d245c52cc368b1c549012d
Dayton Steffeny <daytonsteffeny@mwc-028106.dhcp.missouri.edu>
2018-02-16 15:10:24 -0600

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Dayton Steffeny on 2/16/18.
+//  Copyright © 2018 Dayton Steffeny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
